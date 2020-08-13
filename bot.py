2a8938f8b1832b0da8e4c05660a65620ad5ad5a4
Jakub Tober <jakub.tober@gmail.com>
2020-08-13 17:10:43 +0200

    Typo fix

--- bot.py
+++ bot.py
@@ -5,12 +5,12 @@ MAIN_GIS_URL = r"https://gis.gov.pl/kategoria/zywnosc-i-woda/normy-i-prawo/ostrz
 
 
 if __name__ == "__main__":
-    articles_urls = []
+    article_urls = []
 
     browser = initialize_selenium(MAIN_GIS_URL, 2)
     articles_list = browser.find_element_by_class_name("category-list__content").find_elements_by_class_name("category-list__item")
 
     for article in articles_list:
-        articles_urls.append(article.find_element_by_css_selector("a").get_attribute("href"))
+        article_urls.append(article.find_element_by_css_selector("a").get_attribute("href"))
 
     print("Browser initialized.")
