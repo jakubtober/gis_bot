1882d1e46092fa78fa5acea193ff1c649a240755
Jakub Tober <jakub.tober@gmail.com>
2020-08-14 09:26:28 +0200

    Added beautifulsoup browser

--- utils/selenium_browser.py
+++ utils/selenium_browser.py
@@ -7,16 +7,30 @@ from selenium.webdriver.chrome.options import Options
 CHROMEDRIVER_PATH = r"C:\gis_bot\gis_bot\chromedriver.exe"
 
 
-def initialize_selenium(url, time_to_wait):
-    try:
-        options = Options()
-        options.headless = True
-        browser = webdriver.Chrome(
-            options=options, executable_path=CHROMEDRIVER_PATH)
-        test_request = requests.get(url)
-        browser.get(url)
-        time.sleep(time_to_wait)
-        print("Selenium successfully initialized.")
-        return browser
-    except Exception as error:
-        print("Couldn't initizlize Selenium...")
+class SeleniumBrowser:
+    def __init__(self):
+        self.browser = self._initialize_selenium()
+
+
+    def _initialize_selenium(self):
+        try:
+            options = Options()
+            options.headless = True
+            browser = webdriver.Chrome(
+                options=options, executable_path=CHROMEDRIVER_PATH)
+            time.sleep(2)
+            print("Selenium successfully initialized.")
+            return browser
+        except Exception as error:
+            print("Couldn't initizlize Selenium...")
+
+
+    def get_articles_urls(self, url):
+        articles_list = []
+        articles_list_elements = self.browser.find_element_by_class_name(
+            "category-list__content").find_elements_by_class_name("category-list__item")
+
+        for article in articles_list_elements:
+            articles_list.append(article.find_element_by_css_selector("a").get_attribute("href"))
+
+        return articles_list
