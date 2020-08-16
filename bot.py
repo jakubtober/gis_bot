f3d64f50335af3b92fbdfd9cd063b83cf9cd207a
Jakub Tober <jakub.tober@gmail.com>
2020-08-16 15:47:53 +0200

    Initial db client

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
 from utils.beautiful_soup_browser import BeautifulSoupBrowser
 from utils.facebook_client import FacebookClient
-
+from utils.db_client import DbConnectionClient
 
 
 if __name__ == "__main__":
     beautiful_soup__browser = BeautifulSoupBrowser()
     facebook_client = FacebookClient()
-    all_posts = facebook_client.get_all_posts()
+    db_client = DbConnectionClient()
+
+    all_db_posts = db_client.get_all_db_posts()
 
     article_urls = beautiful_soup__browser._get_articles_urls()
     last_article_url = beautiful_soup__browser._get_last_article_url()
     last_article = beautiful_soup__browser.get_last_article()
 
     message = facebook_client.generate_message_content(
-        last_article.article_title,
-        last_article.article_text,
+        last_article.article_title, last_article.article_text,
     )
+
+    # We will keep part where bot posts message until we have running database
+    # to check history of pubnlished posts first
+
     # facebook_client.publish_post(message, last_article_url)
---
+++ utils/db_client.py
@@ -0,0 +1,34 @@
+import os
+
+try:
+    import psycopg2
+except ImportError as import_exception:
+    print("This script needs psycopg2.\n" f"Exception: {import_exception}")
+except Exception as exception:
+    print(exception)
+
+
+DB_CONNECTION_STRING = "postgres://{}:{}@{}/{}?port={}".format(
+    os.getenv("DB_USER", "xxxxxxxx"),
+    os.getenv("DB_PASSWORD", "xxxxxxxx"),
+    os.getenv("DB_HOST", "xx.xxx.xxx.xxx"),
+    os.getenv("DB_NAME", "xxxxxxx"),
+    os.getenv("DB_PORT", 1234),
+)
+
+
+class DbConnectionClient:
+    def __init__(self):
+        # We will leave it commented until we have db set up and running
+        # if DB_CONNECTION_STRING is None:
+        #     raise AttributeError(
+        #         "Connection string to database is not defined."
+        #     )
+        #
+        # self.db_connection = psycopg2.connect(DB_CONNECTION_STRING)
+        # self.cursor = db_connection.cursor()
+        self.cursor = None
+
+    def get_all_db_posts(self):
+        # Until we have running database we will use mocked data
+        return [("Test title", "Test post text")]
