da38644268d90859abec67fea9d75725efdd517b
Jakub Tober <jakub.tober@gmail.com>
2020-12-04 09:15:56 +0100

    Deleted logging into file
    Deleted logging into file

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ import sys
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-    filename='gis.log'
 )
 
 logger = logging.getLogger()
