9199107c7962052806ff37a9aaf0d3f1165aaf82
Jakub Tober <jakub.tober@gmail.com>
2020-12-07 15:40:55 +0100

    Added database connection
    Added database connection

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import logging
+import os
+
+DB_CONNECTION_URL = os.getenv("DATABASE_URL", "xxxxxxxx")
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
--- utils/db_client.py
+++ utils/db_client.py
@@ -1,35 +1,23 @@
-import os
+from config import DB_CONNECTION_URL
+from config import logger
+
 
 try:
     import psycopg2
 except ImportError as import_exception:
-    print("This script needs psycopg2.\n" f"Exception: {import_exception}")
+    logger.debug("This script needs psycopg2.\n" f"Exception: {import_exception}")
 except Exception as exception:
-    print(exception)
-
-
-DB_CONNECTION_STRING = "postgres://{}:{}@{}/{}?port={}".format(
-    os.getenv("DB_USER", "xxxxxxxx"),
-    os.getenv("DB_PASSWORD", "xxxxxxxx"),
-    os.getenv("DB_HOST", "xx.xxx.xxx.xxx"),
-    os.getenv("DB_NAME", "xxxxxxx"),
-    os.getenv("DB_PORT", 1234),
-)
+    logger.debug(exception)
 
 
 class DbConnectionClient:
     def __init__(self):
-        # It will be commented out when we have db set up and running
-        # if DB_CONNECTION_STRING is None:
-        #     raise AttributeError(
-        #         "Connection string to database is not defined."
-        #     )
-        #
-        # self.db_connection = psycopg2.connect(DB_CONNECTION_STRING)
-        # self.cursor = db_connection.cursor()
-        self.cursor = None
-        self.all_posts = []
+        if DB_CONNECTION_URL is None:
+            raise AttributeError("Connection string to database is not defined.")
 
-    def get_all_db_posts(self):
-        # Until we have running database we will use mocked data
-        return self.all_posts
+        try:
+            self.db_connection = psycopg2.connect(DB_CONNECTION_URL)
+            self.db_cursor = self.db_connection.cursor()
+            logger.debug("Connected to the db.")
+        except Exception as exception:
+            logger.debug(exception)
